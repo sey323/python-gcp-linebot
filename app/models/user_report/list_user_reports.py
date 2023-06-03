9e34dd0605a13ba83dfb1df2a2518c5bb609e377
sey323 <sey323_nm@yahoo.co.jp>
2023-06-03 17:47:09 +0900

    :books: 申告の更新リクエスト追加

--- app/models/user_report/list_user_reports.py
+++ app/models/user_report/list_user_reports.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel, Field
 from app.models.user_report.domain import UserReportModel
 
 
-class UserReportsResponse(BaseModel):
+class ListUserReportResponse(BaseModel):
     user_reports: List[UserReportModel] = Field([], description="ユーザが申告した情報一覧")
--- app/routes/user_report_route.py
+++ app/routes/user_report_route.py
@@ -1,21 +1,31 @@
 from fastapi import APIRouter
-from app.models.user_report.get_user_reports import UserReportsResponse
-from app.models.user_report.post_user_report import (
-    UserReportRequest,
-    UserReportResponse,
+from app.models.user_report.list_user_reports import ListUserReportResponse
+from app.models.user_report.entry_user_report import (
+    EntryUserReportRequest,
+    EntryUserReportResponse,
+)
+from app.models.user_report.update_user_report import (
+    UpdateUserReportRequest,
+    UpdateUserReportResponse,
 )
 
 
 user_report_router = APIRouter(prefix="/report", tags=["report"])
 
 
-@user_report_router.post("", response_model=UserReportResponse)
-async def post_user_report(request: UserReportRequest):
+@user_report_router.post("", response_model=EntryUserReportResponse)
+async def post_user_report(request: EntryUserReportRequest):
     """ユースケース1: ユーザがHelpを申請する"""
-    return UserReportResponse(request_id="sample")
+    return EntryUserReportResponse(request_id="sample")
 
 
-@user_report_router.get("", response_model=UserReportsResponse)
+@user_report_router.put("/{request_id}", response_model=UpdateUserReportResponse)
+async def post_user_report(request_id: str, request: UpdateUserReportRequest):
+    """ユースケース3: ユーザがヘルプ情報を更新する"""
+    return UpdateUserReportResponse(request_id=request_id)
+
+
+@user_report_router.get("", response_model=ListUserReportResponse)
 async def get_user_reports():
     """ユースケース2: 申告内容の一覧を取得する"""
     return []
