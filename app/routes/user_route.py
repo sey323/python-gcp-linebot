39b485879b221714755fefc82ce092716e1ba0f5
FooQoo <horseshoes.buzz.17@gmail.com>
2023-06-11 12:15:20 +0900

    マイナンバー連携のプッシュメッセージを追加

--- app/routes/user_route.py
+++ app/routes/user_route.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
+from app.facades.line_bot.line_message import push_message
 
 from app.models.user.update_user_report import (
     UpdateUserRequest,
     UpdateUserResponse,
+)
+from linebot.models import (
+    TextSendMessage
 )
 from app.services.user import put_user_service
 
@@ -19,4 +23,6 @@ async def put_user(
 ):
     """ユーザのヘルプにコメントをつける"""
     line_user_id = await put_user_service.execute(line_user_id, request)
+    push_message(line_user_id, TextSendMessage(text="マイナンバーの情報連携が完了しました"))
+
     return UpdateUserResponse(user_id=line_user_id)
